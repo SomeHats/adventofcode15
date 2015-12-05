f2438677493cc852278a4812c49f831d8d6dba84
Joe Dytrych <j@dytry.ch>
2015-12-05 17:43:41 +0000

    Day 5: Doesn't He Have Intern-Elves For This?

---
+++ src/adventofcode15/05.clj
@@ -0,0 +1,22 @@
+(ns adventofcode15.05
+  (:gen-class)
+  (:require [adventofcode15.util :refer :all]
+            [clojure.string :as str]))
+
+(defn old-nice?
+  "Returns true if a string is nice, false if naughty"
+  [string]
+  (and (<= 3 (count (re-seq #"[aeiou]" string))) ; at least three vowels
+       (re-find #"(.)\1" string) ; Repeated letter
+       (not (re-find #"ab|cd|pq|xy" string)))) ; doesnt contain the naughty strings
+
+(defn new-nice?
+  "Is the string nice according to the new less completely ridiculous rules?"
+  [string]
+  (and (re-find #"(..).*\1" string) ; Pair of letters appearing twice
+       (re-find #"(.).\1" string))) ; Letter repeating with another letter in between
+
+(defpuzzle "Day 5: Doesn't He Have Intern-Elves For This?"
+  (let [strings (str/split (ask "Gimme dem strings") #"\n")]
+    {:old-rules (count (filter old-nice? strings))
+     :new-rules (count (filter new-nice? strings))}))
